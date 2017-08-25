f7890f05e3bbf8bd70ba77e9a038d2bd1d861822
adunato <daniele.torrigiani@gmail.com>
2017-08-25 14:33:37 +0100

    builders pickup

--- role.nomad_harvester.js
+++ role.nomad_harvester.js
@@ -8,7 +8,7 @@ var nomad_harvester = {
             var harvestRoomName = Game.flags["harvest1"].pos.roomName;
             //room is not visible
             if (typeof harvestRoom == 'undefined') {
-                creep.moveTo(Game.map.findExit(spawnRoom.name, harvestRoomName));
+                creep.moveTo(Game.flags["harvest1"]);
             } else {
                 var sources = harvestRoom.find(FIND_SOURCES);
                 if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
