359e177eacf2862c93063fc0d2511c6fba253604
adunato <daniele.torrigiani@gmail.com>
2017-08-25 11:11:10 +0100

    commit

--- role.upgrader.js
+++ role.upgrader.js
@@ -19,6 +19,7 @@ var roleUpgrader = {
         }
         else {
             var sources = creep.room.find(FIND_SOURCES);
+            Memory.sources = sources;
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
