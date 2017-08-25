aacf5f23af2d061c87568b31cd2b22f02432b83e
adunato <daniele.torrigiani@gmail.com>
2017-08-25 15:29:15 +0100

    rest if containers empty

--- role.nomad_harvester.js
+++ role.nomad_harvester.js
@@ -2,6 +2,7 @@ var nomad_harvester = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
+        creep.memory.selectedSource = null;
         var spawnRoom = Game.spawns["Spawn1"].room;
         if (creep.carry.energy < creep.carryCapacity) {
             if(creep.memory.selectedSource == null) {
