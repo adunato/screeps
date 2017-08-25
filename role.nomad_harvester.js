0008c6a12b60732a2fa1cea3f35d55bdd2fc5808
adunato <daniele.torrigiani@gmail.com>
2017-08-25 14:12:53 +0100

    builders pickup

--- role.nomad_harvester.js
+++ role.nomad_harvester.js
@@ -2,26 +2,39 @@ var nomad_harvester = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-        var room = creep.room;
+        var harvestRoom = Game.flags["harvest1"].room;
+        var spawnRoom = Game.spawns["Spawn1"].room;
         if (creep.carry.energy < creep.carryCapacity) {
-            var sources = room.find(FIND_SOURCES);
+            var sources = harvestRoom.find(FIND_SOURCES);
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
-            var targets = room.find(FIND_STRUCTURES, {
+            var targets = spawnRoom.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION ||
                         structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                        structure.structureType == STRUCTURE_TOWER ) && structure.energy < structure.energyCapacity;
                 }
+            });
+            var containers = spawnRoom.find(FIND_STRUCTURES, {
+                filter: (container) => {
+                    // return (structure.structureType == STRUCTURE_CONTAINER) && structure.store < structure.storeCapacity;
+                    return (container.structureType == STRUCTURE_CONTAINER) && container.store.energy < container.storeCapacity;
+                }
+
             });
             if (targets.length > 0) {
                 if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
-            } else {
+            } else if (containers.length > 0){
+                if (creep.transfer(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(containers[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+            else {
                 creep.moveTo(Game.flags["RestArea"], {visualizePathStyle: {stroke: '#ffffff'}});
                 creep.say("Rest");
             }
