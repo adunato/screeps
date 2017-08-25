6f2cfc073c42cc38b7fe20689ada1315bd5491b4
adunato <daniele.torrigiani@gmail.com>
2017-08-25 08:27:54 +0100

    commit

--- role.harvester.js
+++ role.harvester.js
@@ -20,6 +20,9 @@ var roleHarvester = {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+            } else {
+                creep.moveTo(Game.flags["RestArea"], {visualizePathStyle: {stroke: '#ffffff'}});
+                creep.say("Going to rest");
             }
         }
     }
