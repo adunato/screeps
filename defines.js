b3c23a3e08216535b852cba1d70e9c613249c604
adunato <daniele.torrigiani@gmail.com>
2017-08-26 09:26:35 +0100

    counters

--- defines.js
+++ defines.js
@@ -1,12 +1,12 @@
 var defines = {
 
     initDefines: function () {
-        minSpawn = [
-            ["harvester","7"],
-            ["builder","2"],
-            ["nomad_harvester","5"],
-            ["upgrader","2"]
-        ]
+        minSpawn = {
+            "harvester": 7,
+            "builder": 2,
+            "nomad_harvester": 5,
+            "upgrader": 2
+    }
         MIN_UPGRADERS = 2;
         MIN_BUILDERS = 3;
         MIN_HARVESTERS = 7;
