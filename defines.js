04365a3f142c827c3dedabd6c41b35a14f20e1bc
adunato <daniele.torrigiani@gmail.com>
2017-08-27 17:35:09 +0100

    state machine

--- defines.js
+++ defines.js
@@ -2,29 +2,28 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleNomadHarvester = require('role.nomad_harvester');
-var minSpawn, bodyParts, modules;
 var defines = {
 
     initDefines: function () {
-        minSpawn = {
+        global.minSpawn = {
             "harvester": 1,
             "builder": 1,
             "nomad_harvester": 0,
             "upgrader": 0
         };
-        bodyParts = {
+        global.bodyParts = {
             "harvester": [WORK,CARRY,MOVE],
             "builder": [WORK,CARRY,MOVE],
             "nomad_harvester": [WORK,CARRY,MOVE],
             "upgrader": [WORK,CARRY,MOVE]
         };
-        modules = {
+        global.modules = {
             "harvester": roleHarvester,
             "builder": roleBuilder,
             "nomad_harvester": roleNomadHarvester,
             "upgrader": roleUpgrader
         };
-        console.log("harvester" + minSpawn["harvester"]);
+        console.log("harvester" + global.minSpawn["harvester"]);
     }
 
 }
