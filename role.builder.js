365f74750e04b6d0cb73de548f6b59bd99bdb0bf
adunato <daniele.torrigiani@gmail.com>
2017-08-27 18:13:34 +0100

    state machine

--- role.builder.js
+++ role.builder.js
@@ -1,56 +1,56 @@
 var StateMachine = require('state-machine')
-var builderFSM = new StateMachine({
-    init: 'withdraw',
-    transitions: [
-        { name: 'energyEmpty', from: 'build',  to: 'withdraw' },
-        { name: 'energyFull', from: 'withdraw', to: 'build'  },
-        { name: 'containersEmpty', from: 'withdraw', to: 'rest'  },
-    ],
-    data: function(creep) {
-        return {
-            creep: creep
-        }
-    },
-    methods: {
-        onWithdraw:     function() {
-            creep.withdrawEnergy();
-        },
-        onBuild:     function() {
-            creep.build();
-        }
-    }
-});
+// var builderFSM = new StateMachine({
+//     init: 'withdraw',
+//     transitions: [
+//         { name: 'energyEmpty', from: 'build',  to: 'withdraw' },
+//         { name: 'energyFull', from: 'withdraw', to: 'build'  },
+//         { name: 'containersEmpty', from: 'withdraw', to: 'rest'  },
+//     ],
+//     data: function(creep) {
+//         return {
+//             creep: creep
+//         }
+//     },
+//     methods: {
+//         onWithdraw:     function() {
+//             creep.withdrawEnergy();
+//         },
+//         onBuild:     function() {
+//             creep.build();
+//         }
+//     }
+// });
 
-Creep.prototype.withdrawEnergy = function() {
-    var containers = room.find(FIND_STRUCTURES, {
-        filter: (container) => {
-            // return (structure.structureType == STRUCTURE_CONTAINER) && structure.store < structure.storeCapacity;
-            return (container.structureType == STRUCTURE_CONTAINER) && container.store.energy > 0;
-        }
-
-    });
-
-    if(containers.length > 0) {
-        if (this.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            this.moveTo(containers[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-        }
-    } else {
-        this.moveTo(Game.flags["RestArea"], {visualizePathStyle: {stroke: '#ffffff'}});
-        this.say("Rest");
-    }
-};
-
-Creep.prototype.build = function() {
-    var targets = this.room.find(FIND_CONSTRUCTION_SITES);
-    if(targets.length) {
-        if(this.build(targets[0]) == ERR_NOT_IN_RANGE) {
-            this.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-        }
-    } else {
-        this.moveTo(Game.flags["RestArea"], {visualizePathStyle: {stroke: '#ffffff'}});
-        this.say("Rest");
-    }
-};
+// Creep.prototype.withdrawEnergy = function() {
+//     var containers = room.find(FIND_STRUCTURES, {
+//         filter: (container) => {
+//             // return (structure.structureType == STRUCTURE_CONTAINER) && structure.store < structure.storeCapacity;
+//             return (container.structureType == STRUCTURE_CONTAINER) && container.store.energy > 0;
+//         }
+//
+//     });
+//
+//     if(containers.length > 0) {
+//         if (this.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+//             this.moveTo(containers[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+//         }
+//     } else {
+//         this.moveTo(Game.flags["RestArea"], {visualizePathStyle: {stroke: '#ffffff'}});
+//         this.say("Rest");
+//     }
+// };
+//
+// Creep.prototype.build = function() {
+//     var targets = this.room.find(FIND_CONSTRUCTION_SITES);
+//     if(targets.length) {
+//         if(this.build(targets[0]) == ERR_NOT_IN_RANGE) {
+//             this.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+//         }
+//     } else {
+//         this.moveTo(Game.flags["RestArea"], {visualizePathStyle: {stroke: '#ffffff'}});
+//         this.say("Rest");
+//     }
+// };
 
 
 var roleBuilder = {
@@ -58,18 +58,18 @@ var roleBuilder = {
     run: function(creep) {
         // console.log(creep.name);
         //builderFSM.setState(creep.memory.state);
-        if(creep.carry.energy == 0){
-            try {
-                builderFSM.energyEmpty();
-            }
-            catch(err){
-                console.log("error: " + err);
-            }
-        }
-        if(creep.carry.energy == creep.carryCapacity){
-            builderFSM.energyFull();
-        }
-        creep.memory.state = builderFSM.state;
+        // if(creep.carry.energy == 0){
+        //     try {
+        //         builderFSM.energyEmpty();
+        //     }
+        //     catch(err){
+        //         console.log("error: " + err);
+        //     }
+        // }
+        // if(creep.carry.energy == creep.carryCapacity){
+        //     builderFSM.energyFull();
+        // }
+        // creep.memory.state = builderFSM.state;
     }
 };
 
