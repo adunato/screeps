5c68a0f5898df7eabd75406e745a72c82c6e55ce
adunato <daniele.torrigiani@gmail.com>
2017-08-27 18:23:59 +0100

    state machine

--- role.builder.js
+++ role.builder.js
@@ -14,8 +14,9 @@ var builderFSM = new StateMachine.factory({
     methods: {
         onWithdraw:     function() {
             var creep = Game.creeps[this.creepName];
-            if(typeof creep != "undefined")
+            if(typeof creep != "undefined") {
                 creep.withdrawEnergy();
+            }
         },
         onBuild:     function() {
             //this.creep.build();
@@ -23,24 +24,24 @@ var builderFSM = new StateMachine.factory({
     }
 });
 
-// Creep.prototype.withdrawEnergy = function() {
-//     var containers = room.find(FIND_STRUCTURES, {
-//         filter: (container) => {
-//             // return (structure.structureType == STRUCTURE_CONTAINER) && structure.store < structure.storeCapacity;
-//             return (container.structureType == STRUCTURE_CONTAINER) && container.store.energy > 0;
-//         }
-//
-//     });
-//
-//     if(containers.length > 0) {
-//         if (this.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-//             this.moveTo(containers[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-//         }
-//     } else {
-//         this.moveTo(Game.flags["RestArea"], {visualizePathStyle: {stroke: '#ffffff'}});
-//         this.say("Rest");
-//     }
-// };
+Creep.prototype.withdrawEnergy = function() {
+    var containers = room.find(FIND_STRUCTURES, {
+        filter: (container) => {
+            // return (structure.structureType == STRUCTURE_CONTAINER) && structure.store < structure.storeCapacity;
+            return (container.structureType == STRUCTURE_CONTAINER) && container.store.energy > 0;
+        }
+
+    });
+
+    if(containers.length > 0) {
+        if (this.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            this.moveTo(containers[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+        }
+    } else {
+        this.moveTo(Game.flags["RestArea"], {visualizePathStyle: {stroke: '#ffffff'}});
+        this.say("Rest");
+    }
+};
 //
 // Creep.prototype.build = function() {
 //     var targets = this.room.find(FIND_CONSTRUCTION_SITES);
