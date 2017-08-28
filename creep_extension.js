16161c572ecefe4e35475ef68231c40bbe137d24
adunato <daniele.torrigiani@gmail.com>
2017-08-28 23:19:39 +0100

    state machine

--- creep_extension.js
+++ creep_extension.js
@@ -3,7 +3,7 @@ Creep.prototype.withdrawEnergy = function () {
     var containers = cache.findContainers(this.room);
     if (containers.length > 0) {
         if (this.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            this.moveTo(containers[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            this.moveTo(containers[0], {visualizePathStyle: {stroke: '#0027ff'}});
         }
     }
 };
@@ -12,7 +12,7 @@ Creep.prototype.harvestEnergy = function () {
     var sources = cache.findSources(this.room);
     if (sources.length > 0) {
         if (this.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-            this.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            this.moveTo(sources[0], {visualizePathStyle: {stroke: '#0027ff'}});
         }
     }
 };
@@ -21,7 +21,7 @@ Creep.prototype.dropEnergy = function () {
     var structures = cache.findEnergyContainers(this.room);
     if (structures.length > 0) {
         if (this.transfer(structures[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            this.moveTo(structures[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            this.moveTo(structures[0], {visualizePathStyle: {stroke: '#ffda00'}});
         }
     }
 };
@@ -30,7 +30,7 @@ Creep.prototype.feedEnergy = function () {
     var structures = cache.findEnergyFedStructures(this.room);
     if (structures.length > 0) {
         if (this.transfer(structures[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            this.moveTo(structures[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            this.moveTo(structures[0], {visualizePathStyle: {stroke: '#ffe21f'}});
         }
     }
 };
@@ -39,7 +39,7 @@ Creep.prototype.buildConstruction = function () {
     var constructionSites = cache.findConstructionSites(this.room);
     if (constructionSites.length) {
         if (this.build(constructionSites[0]) == ERR_NOT_IN_RANGE) {
-            this.moveTo(constructionSites[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            this.moveTo(constructionSites[0], {visualizePathStyle: {stroke: '#14ff00'}});
         }
     }
 };
