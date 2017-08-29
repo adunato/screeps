423b7204860fba28fcdaf0a00d752623e3a77c24
adunato <daniele.torrigiani@gmail.com>
2017-08-29 07:59:12 +0100

    state machine

--- role.nomad_harvester.js
+++ role.nomad_harvester.js
@@ -5,3 +5,11 @@ function NomadHarvester(creep) {
 
 NomadHarvester.prototype = Object.create(Creep.prototype);
 NomadHarvester.prototype.constructor = NomadHarvester;
+
+NomadHarvester.prototype.harvestEnergy = function () {
+    this.say("I'm nomad!")
+    var source = Game.getObjectById(this.memory.selectedSource);
+    if (this.harvest(source) == ERR_NOT_IN_RANGE) {
+        this.moveTo(source, {visualizePathStyle: {stroke: '#0027ff'}});
+    }
+};
