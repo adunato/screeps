b2824b7cc69325808dd9074412d407cc1e95e649
adunato <daniele.torrigiani@gmail.com>
2017-08-31 19:32:23 +0100

    squads

--- main.js
+++ main.js
@@ -120,8 +120,7 @@ function createSquad(squadRole, squadName) {
 function createSquads() {
     for (var flag in Game.flags) {
         for (var squadRole in global.squadProfiles) {
-            console.log(flag);
-            if (checkSquadFromFlag(squadRole, flag.name)) {
+            if (checkSquadFromFlag(squadRole, flag)) {
                 global.squads[squadRole] = createSquad(squadRole, flag.name);
             }
         }
