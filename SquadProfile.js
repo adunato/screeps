6bac0a03dfeb164dbbc7e4d7597a82878dffccf8
adunato <daniele.torrigiani@gmail.com>
2017-09-01 07:38:39 +0100

    squads

--- SquadProfile.js
+++ SquadProfile.js
@@ -1,12 +1,15 @@
 function SquadProfile(name) {
+    //e.g. [["harvester",3], ["builder",1]]
     this.squadProfile = global.squadProfiles[name];
     this.name = name;
 };
 
 SquadProfile.prototype.getCreepQuantity = function(role) {
-    for(role in this.squadProfile){
-        if(role[0] === role)
-            return role[1];
+    for(var i = 0; i <  this.squadProfile.length; i++){
+        //e.g. squadRole = [["harvester",3]
+        var squadRole = this.squadProfile[i];
+        if(squadRole[0] === role)
+            return squadRole[1];
     }
     return 0;
 };
