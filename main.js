fa5501f4656a89006ab133174cb55a397b7a01d7
adunato <daniele.torrigiani@gmail.com>
2017-09-01 07:51:12 +0100

    squads

--- main.js
+++ main.js
@@ -106,12 +106,13 @@ function checkSquadFromFlag(role, flagName) {
         var flagID = flagName;
         flagID.replace(role, "");
         if (!squads[role]) {
-            console.log("Squad role " + role + " needed")
+            console.log("Squad role " + role + " needed");
+            console.log("Current squads size " + squads.length);
             squads[role] = new Array();
             return true;
         }
         else if (squads[role].length < flagID) {
-            console.log("Squad " + flagName + " needed")
+            console.log("Squad " + flagName + " needed");
             return true;
         } else {
             return false
