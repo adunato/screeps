15cfe5a6daa44366214dbd119f0dd100c1c1a58f
adunato <daniele.torrigiani@gmail.com>
2017-09-02 10:38:04 +0100

    push

--- creep_extension.js
+++ creep_extension.js
@@ -46,7 +46,7 @@ Creep.prototype.nomad_harvester_selectSource = function () {
 };
 
 Creep.prototype.harvestEnergy = function () {
-    console.log("harvestEnergy");
+    console.log("creep.harvestEnergy");
     var source = Game.getObjectById(this.memory.selectedSource);
     if (this.harvest(source) == ERR_NOT_IN_RANGE) {
         this.moveTo(source, {visualizePathStyle: {stroke: '#0027ff'}});
