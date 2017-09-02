81a2d5e16c3c2a7e563ad6bfed4f60fbfd3b4577
adunato <daniele.torrigiani@gmail.com>
2017-09-02 09:45:42 +0100

    squads

--- role.harvester.js
+++ role.harvester.js
@@ -3,7 +3,7 @@ var cache = require('cache');
 var harvesterFSM = new StateMachine.factory({
     init: 'none',
     transitions: [
-        {name: 'energyEmpty', from: ['none','dropEnergy', 'rest', 'feeEnergy', 'selectSource'], to: 'selectSource'},
+        {name: 'energyEmpty', from: ['none','dropEnergy', 'rest', 'feedEnergy', 'selectSource'], to: 'selectSource'},
         {name: 'sourceSelected', from: ['selectSource', 'harvestEnergy'], to: 'harvestEnergy'},
         {name: 'energyFull', from: ['feedEnergy', 'harvestEnergy'], to: 'feedEnergy'},
         {name: 'energyFedStructuresFull', from: ['dropEnergy','feedEnergy'], to: 'dropEnergy'},
