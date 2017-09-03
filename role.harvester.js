0a9a71ad6c592e0bb3f86468b63405d08dbbd722
adunato <daniele.torrigiani@gmail.com>
2017-09-03 09:24:50 +0100

    PPPP

--- role.harvester.js
+++ role.harvester.js
@@ -71,6 +71,7 @@ var roleHarvester = {
         if (typeof creepState === "undefined")
             creepState = "none";
         var stateMachine = new harvesterFSM(creep.name);
+        console.log(visualize(stateMachine));
         stateMachine.goto(creepState);
         if (creep.carry.energy < creep.carryCapacity &&  !creep.memory.selectedSource && stateMachine.can("energyEmpty")) {
             stateMachine.energyEmpty();
