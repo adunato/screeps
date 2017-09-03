e4a3d58a6ba0cfbbdfd7bf9cd1f9f03925e704d4
adunato <daniele.torrigiani@gmail.com>
2017-09-03 22:14:24 +0100

    PPPP

--- main.js
+++ main.js
@@ -68,12 +68,14 @@ function manageDefense() {
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if (closestHostile) {
             tower.attack(closestHostile);
+            return;
         }
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
         });
         if (closestDamagedStructure) {
             tower.repair(closestDamagedStructure);
+
         }
 
     }
