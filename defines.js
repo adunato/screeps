1c5040d4bf93aadfdd8980c4027b9c26a3ef1253
adunato <daniele.torrigiani@gmail.com>
2017-09-04 10:50:09 +0100

    PP

--- defines.js
+++ defines.js
@@ -28,7 +28,7 @@ var defines = {
 
     initDefines: function () {
         global.bodyParts = {
-            "harvester": [MOVE,MOVE,MOVE,MOVE,WORK,WORK,WORK,WORK,CARRY],
+            "harvester": [MOVE,MOVE,MOVE,MOVE,MOVE,WORK,WORK,WORK,WORK,WORK,CARRY],
             "builder": [MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY],
             "nomad_harvester": [WORK, CARRY, MOVE],
             "upgrader": [MOVE,MOVE,MOVE,MOVE,MOVE,WORK,WORK,WORK,WORK,WORK,CARRY],
