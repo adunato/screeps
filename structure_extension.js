00e7635a70cd2fc92a1a543748166aab2aae4364
adunato <daniele.torrigiani@gmail.com>
2017-09-05 19:57:20 +0100

    commit

--- structure_extension.js
+++ structure_extension.js
@@ -1,5 +1,6 @@
 //var cache = require('cache');
 Structure.prototype.transfer = function (target, resourceType) {
+    console.log("I have been here");
     if(typeof(target) === 'Creep'){
         target.withdraw(this,resourceType);
     }
