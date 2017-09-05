3838349349fece3278170cd4b26234d99d0593b2
adunato <daniele.torrigiani@gmail.com>
2017-09-05 20:19:00 +0100

    commit

--- structure_extension.js
+++ structure_extension.js
@@ -1,5 +1,5 @@
-StructureContainer.prototype.transfer = function (target, resourceType) {
-    if(typeof(target) === 'Creep'){
-        return target.withdraw(this,resourceType);
-    }
-};
+// StructureContainer.prototype.transfer = function (target, resourceType) {
+//     if(typeof(target) === 'Creep'){
+//         return target.withdraw(this,resourceType);
+//     }
+// };
