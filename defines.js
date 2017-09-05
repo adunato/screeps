f13722f4724682cec33f96a2e4e18bd2d96d180f
adunato <daniele.torrigiani@gmail.com>
2017-09-05 10:20:06 +0100

    PP

--- defines.js
+++ defines.js
@@ -137,7 +137,7 @@ var defines = {
         global.destinationContainers = [
             '59ae529fbd28427f15402b36',//tower
             '59aab3b0b08c1b0fa81a4370',//central
-            '59ae5c75496afa193d2d8a40',//RC
+            // '59ae5c75496afa193d2d8a40',//RC
         ];
     }
 
