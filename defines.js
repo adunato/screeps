b57ddd57e0dc51fd5e678340176c64d23a91fca9
adunato <daniele.torrigiani@gmail.com>
2017-09-06 14:02:28 +0100

    commit

--- defines.js
+++ defines.js
@@ -137,8 +137,8 @@ var defines = {
             '59ae4aea33f29c3b855f02c0',
         ];
         global.destinationContainers = [
-            '59ae529fbd28427f15402b36',//tower
             '59aab3b0b08c1b0fa81a4370',//central
+            '59ae529fbd28427f15402b36',//tower
             '59ae5c75496afa193d2d8a40',//RC
         ];
     }
