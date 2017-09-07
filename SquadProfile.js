fe0f3effa09fde54eace31de8d82e21a774ad60d
adunato <daniele.torrigiani@gmail.com>
2017-09-07 13:30:53 +0100

    commit

--- SquadProfile.js
+++ SquadProfile.js
@@ -1,15 +1,9 @@
 function SquadProfile(name) {
     //e.g. [["harvester",3], ["builder",1]]
     this.squadProfile = global.squadProfiles[name];
-    if(!this.squadProfile){
-        console.log("error: " + name);
-    }
     this.name = name;
 }
 SquadProfile.prototype.getCreepQuantity = function(role) {
-    if(!this.squadProfile){
-        console.log("error: " + this.name);
-    }
     for(var i = 0; i <  this.squadProfile.length; i++){
         //e.g. squadRole = [["harvester",3]
         var squadRole = this.squadProfile[i];
