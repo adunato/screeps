b0e48591dbf4acb1af2d5b9710094b1165d083d7
adunato <daniele.torrigiani@gmail.com>
2017-09-10 07:52:51 +0100

    commit

--- creep_extension.js
+++ creep_extension.js
@@ -350,17 +350,25 @@ Creep.prototype.buildConstruction = function () {
 };
 
 Creep.prototype.repairConstruction = function (minRepairLevelPc) {
-    var flag = Game.flags[this.memory.squad];
-    //move to flag if not in flag's room
-    if (flag != null && (!flag.room || flag.room.name != this.room.name)) {
-        this.moveTo(flag, {visualizePathStyle: {stroke: '#ffda00'}});
-        return;
+    // var flag = Game.flags[this.memory.squad];
+    // //move to flag if not in flag's room
+    // if (flag != null && (!flag.room || flag.room.name != this.room.name)) {
+    //     this.moveTo(flag, {visualizePathStyle: {stroke: '#ffda00'}});
+    //     return;
+    // }
+    let construction = this.memory.repairConstruction;
+    if(!construction){
+        var repairConstructions = cache.findRepairStructures(this.room, minRepairLevelPc);
+        if (repairConstructions.length) {
+            construction = this.getNearestObjectByDistance(repairConstructions);
+        }
     }
-    var repairConstructions = cache.findRepairStructures(this.room, minRepairLevelPc);
-    if (repairConstructions.length) {
-        var construction = this.getNearestObjectByDistance(repairConstructions);
-        if (this.repair(construction) == ERR_NOT_IN_RANGE) {
+    if(construction) {
+        this.memory.repairConstruction = construction;
+        if (this.repair(construction) === ERR_NOT_IN_RANGE) {
             this.moveTo(construction, {visualizePathStyle: {stroke: '#14ff00'}});
+        } else if(construction.hits === construction.hitsMax) {
+            this.memory.repairConstruction = null;
         }
     }
 };
