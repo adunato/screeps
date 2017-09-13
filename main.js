3cab3c2437653fc473724560e31124a2cda4cda3
adunato <daniele.torrigiani@gmail.com>
2017-09-13 21:20:23 +0100

    commit

--- main.js
+++ main.js
@@ -342,7 +342,7 @@ module.exports.loop = function () {
     createSquads();
     logCPU('createSquads ');
     assignCreepsToSquads();
-    Memory.squads = global.squadsIndex;
+    // Memory.squads = global.squadsIndex;
     logCPU('assignCreepsToSquads ');
     logSpawing();
     logCPU('logSpawing ');
