7fe72c43dfb1756c44142b912b2c957ffa39cd34
adunato <daniele.torrigiani@gmail.com>
2017-09-13 21:20:07 +0100

    commit

--- creep_extension.js
+++ creep_extension.js
@@ -443,7 +443,7 @@ Creep.prototype.upgradeController_ = function () {
 Creep.prototype.rest = function () {
     this.memory.selectedSource = null;
     var pinnedToRoom = false;
-    console.log("global.squadsIndex " + global.squadsIndex[this.memory.squadName]);
+    console.log("global.squadsIndex " + global.squadsIndex['BU1']);
     if(this.getSquad()){
         console.log("has squad")
         pinnedToRoom = this.getSquad().isPinnedToFlag();
