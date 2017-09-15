68a670719e4219dbb0f1b313bc8755968ccaf746
adunato <daniele.torrigiani@gmail.com>
2017-09-15 22:34:15 +0100

    commit

--- creep_extension.js
+++ creep_extension.js
@@ -447,7 +447,7 @@ Creep.prototype.multiFunction = function () {
         var spawns = cache.findSpawnsWithEnergy(this.room);
         if(spawns.length > 0){
             var spawn = spawns[0];
-            if(spawn.energy < 300){
+            if(spawn.energy < spawn.energyCapacity){
                 this.dropEnergy({DROP_STRUCTURE : true});
                 return;
             }
