90d0d928ed0b7e9cf29e3529a2b190df56f0ec95
adunato <daniele.torrigiani@gmail.com>
2017-09-16 13:41:01 +0100

    commit

--- defines.js
+++ defines.js
@@ -63,8 +63,8 @@ var defines = {
             "nomad_harvester": [WORK, CARRY, MOVE],
             "upgrader": [
                 [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK, CARRY],
-                [MOVE, MOVE, MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, CARRY],
-                [MOVE, MOVE, MOVE, MOVE, WORK, WORK, WORK, CARRY],
+                [MOVE, MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK, CARRY],
+                [MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, CARRY],
                 // [MOVE, MOVE, MOVE, WORK, CARRY, CARRY],
                 // [MOVE, MOVE, WORK, CARRY],
             ],
