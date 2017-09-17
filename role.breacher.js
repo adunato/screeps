61318bd00b580dba0ad7aaed4cbc4c1c56ce4da8
adunato <daniele.torrigiani@gmail.com>
2017-09-17 09:57:41 +0100

    commit

--- role.breacher.js
+++ role.breacher.js
@@ -1,5 +1,5 @@
 var statemachine = require('state-machine');
-const CREEP_DAMAGE_LIMIT = 0;
+const CREEP_DAMAGE_LIMIT = 90;
 var breacherFSM = new statemachine.StateMachine.factory({
     init: 'none',
     transitions: [
