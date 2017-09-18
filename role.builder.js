922b0aede1a5e44651a86a494236701dc33d7959
adunato <daniele.torrigiani@gmail.com>
2017-09-18 22:39:05 +0100

    commit

--- role.builder.js
+++ role.builder.js
@@ -28,7 +28,7 @@ var builderFSM = new statemachine.StateMachine.factory({
         onWithdraw: function () {
             var creep = Game.creeps[this.creepName];
             console.log(creep.name + " " + cache.getStoredEnergy(creep.room));
-            if(cache.getStoredEnergy(creep.room) > 0)
+            if(cache.getStoredEnergy(creep.room) >= 0)
                 creep.withdrawEnergy();
             else
                 creep.rest();
