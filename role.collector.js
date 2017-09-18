9623a58c7020ceb90fe092a008778211c16511c6
adunato <daniele.torrigiani@gmail.com>
2017-09-18 22:20:46 +0100

    commit

--- role.collector.js
+++ role.collector.js
@@ -6,7 +6,6 @@ var collectorFSM = new statemachine.StateMachine.factory({
         {name: 'energyEmpty', from: ['none', 'rest', 'collectEnergy'], to: 'collectEnergy'},
         {name: 'energyFull', from: ['collectEnergy', 'rest','dropEnergy','reDropEnergy'], to: 'dropEnergy'},
         {name: 'reDropEnergy', from: ['dropEnergy','reDropEnergy'], to: 'reDropEnergy'},
-        {name: 'noSource', from: ['collectEnergy', 'rest'], to: 'rest'},
         {name: 'nowhereToDrop', from: ['reDropEnergy','dropEnergy','rest'], to: 'rest'},
         {name: 'timeToDie', from: ['*'], to: 'suicide'},
         {name: 'timeToDie', from: ['collectEnergy', 'suicide'], to: 'suicide'},
@@ -33,10 +32,6 @@ var collectorFSM = new statemachine.StateMachine.factory({
         onReDropEnergy: function () {
             var creep = Game.creeps[this.creepName];
             creep.dropEnergy({DROP_CONTAINER : true,DROP_STRUCTURE : true, DROP_CARRIER : true});
-        },
-        onNoSource: function () {
-            var creep = Game.creeps[this.creepName];
-            creep.rest();
         },
         onNowhereToDrop: function () {
             var creep = Game.creeps[this.creepName];
@@ -74,9 +69,6 @@ var roleHarvester = {
         }
         if (creep.carry.energy < creep.carryCapacity && stateMachine.can("reDropEnergy")) {
             stateMachine.reDropEnergy();
-        }
-        if (cache.findSources(creep.room).length === 0 && stateMachine.can("noSource")) {
-            stateMachine.noSource();
         }
         if (cache.findEmptyPlaceToDropStuff(creep.room, {DROP_CONTAINER : true,DROP_STRUCTURE : true, DROP_CARRIER : true}).length === 0 && stateMachine.can("nowhereToDrop")) {
             stateMachine.nowhereToDrop();
