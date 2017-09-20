a48966a0c86225ca06bf28f8190be74d3256cb35
adunato <daniele.torrigiani@gmail.com>
2017-09-20 17:39:18 +0100

    commit

--- role.transporter.js
+++ role.transporter.js
@@ -1,6 +1,6 @@
 var statemachine = require('state-machine');
 var cache = require('cache');
-var MIN_SOURCE_CONTAINER_QUANTITY_PC = 50;
+var MIN_SOURCE_CONTAINER_QUANTITY_PC = 25;
 var MAX_DESTINATION_CONTAINER_QUANTITY_PC = 90;
 var carrierFSM = new statemachine.StateMachine.factory({
     init: 'none',
