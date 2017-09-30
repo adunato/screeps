20893a6a9f77cd6fe26b4524fa53332be8edec7b
adunato <daniele.torrigiani@gmail.com>
2017-09-30 17:01:33 +0100

    commit

--- creep_extension.js
+++ creep_extension.js
@@ -35,7 +35,7 @@ Creep.prototype.withdrawEnergyFromSources = function (energySources) {
             return false;
         }
         var resourceType = RESOURCE_ENERGY;
-        console.log("energySource.store: " + energySource.store.size());
+        console.log("energySource.store: " + energySource.store);
         // if(energySource.store)
         // resourceType = _.findKey(energySource.store, (amt,key) => amt > 0 && key !== RESOURCE_ENERGY);
         var res = energySource.transfer(this, resourceType);
