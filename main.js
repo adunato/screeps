5560230c8b771d2e395bc6c7634e65799c4bdecb
adunato <daniele.torrigiani@gmail.com>
2017-10-02 13:42:00 +0100

    commit

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ function spawn(roleName, squad) {
         spawnSlots[selectedSpawn.name] = roleName;
 
     for(var spawnName in cache.findSpawns(selectedSpawn.room)){
-        console.log(spawnName);
+        console.log(selectedSpawn.room);
         var spawn = Game.spawns[spawnName];
         if(spawn.spawning !== null ) {
             var spawningCreep = Game.creeps[spawn.spawning.name];
