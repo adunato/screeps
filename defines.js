03a9f0c00da9b01fb7bf409077bcbe1914a607ff
adunato <daniele.torrigiani@gmail.com>
2017-10-05 08:22:19 +0100

    commit

--- defines.js
+++ defines.js
@@ -43,7 +43,7 @@ var defines = {
                     // [MOVE, MOVE,WORK, WORK, WORK, WORK, WORK, CARRY, CARRY],
                     // [MOVE, MOVE, MOVE, MOVE, MOVE, WORK, WORK, WORK, CARRY, CARRY],
                     // [MOVE, MOVE, MOVE,MOVE, WORK, WORK, CARRY, CARRY],
-                    [MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK, WORK, CARRY]
+                    [MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK, WORK, CARRY],
                     [MOVE, MOVE,MOVE, WORK, CARRY, CARRY],
                     [MOVE, MOVE, WORK, CARRY]
                 ],
